50d9b6939dccc42bfb59489fe3d87696ee35370d
prindia <nminingo@gmu.edu>
2022-10-18 18:55:57 -0400

    updare rxn and stim files

---
+++ test/tot_species.py
@@ -0,0 +1,4 @@
+sub_species={'COF':['Cof','Cofactin','pLIMKCof'], 'kal':['Kal','pKal','KalCKpCamCa4','KalCKCamCa4','KalPKAc','pKalPP1','pKalRac']}
+weight={}
+             
+tot_species=['COF','kal']
