d0adad1de5fa5cbe964beb30313fc1f58a6bde7c
prindia <nminingo@gmu.edu>
2023-03-13 12:08:32 -0400

    first working model that fit data

--- tot_species.py
+++ tot_species.py
@@ -1,4 +1,4 @@
-sub_species={'Kal':['Kal','pKal','KalCKpCamCa4','KalCKCamCa4','KalPKAc','pKalRac'],['PKA':['PKA','PKAc','PKAr','KalPKAc','PKAcLIMK','PKAc_PDE4_cAMP','PKAcPDE4','PKAcLR','PKAcpLR','PKAcppLR','PKAcAMP2','PKAcAMP4']}
+sub_species={'Kal':['Kal','pKal','KalCKpCamCa4','KalCKCamCa4','KalPKAc','pKalRac'],'PKA':['PKA','PKAc','PKAr','KalPKAc','PKAcLIMK','PKAc_PDE4_cAMP','PKAcPDE4','PKAcLR','PKAcpLR','PKAcppLR','PKAcAMP2','PKAcAMP4'],'Cof':['Cof','Cofactin','pLIMKCof','pCof','SSHpCof']}
 
-tot_species=['PKA']
+tot_species=['PKA','Cof']
 weight={}
